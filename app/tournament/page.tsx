2c5e0a4672adae38bfedd5037e6cc2c1a2741aa8
Jargal <nbjargal@gmail.com>
2025-02-02 12:01:21 -0500

    change title

--- app/tournament/page.tsx
+++ app/tournament/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="container mx-auto py-8">
       <h1 className="text-3xl font-bold mb-8 text-center">
-        Table Tennis Tournament
+        DC MONGOL TABLE TENNIS
       </h1>
       <TournamentApp />
     </main>
