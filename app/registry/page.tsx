cfad8ae1763716f7b0f0512fc5b2e50f290aa936
Jargal <nbjargal@gmail.com>
2025-03-21 16:25:43 -0400

    add: garaar burtgel

---
+++ app/registry/page.tsx
@@ -0,0 +1,11 @@
+
+import TableTennisRegistration from '@/components/TableTennisRegistration'
+import React from 'react'
+
+function page() {
+  return (
+    <TableTennisRegistration />
+  )
+}
+
+export default page
